8d831b7723ddf3f12040cd947b0ab01b445c86a9
Vikas Chugh <vikaschugh84@gmail.com>
2022-02-01 19:18:04 +0530

    Slightly Tweaking in files

--- Ruby/Ruby Programs/employee_role.rb
+++ Ruby/Ruby Programs/employee_role.rb
@@ -1,3 +1,5 @@
+# To understand Instance Variable and Instance Methods Basic Workflow
+
 class Employee 
   def initialize (name,role)
     @name = name
@@ -15,6 +17,10 @@ Ranjit = Employee.new('Ranjit','QA Engineer')
 
 Vikram = Employee.new('Vikram','Software Developer')
 
+# Puts Vikram.role - Results as 'Vikram working here as Software Developer'
+
 Ranjit.role 
 Vikram.role
+
+# Here the different objects can access the single method as stated above 'Ranjit.role' and 'Vikram.role' and their output stored in @name and @role instance variables for specific object and returns us the value.
 
