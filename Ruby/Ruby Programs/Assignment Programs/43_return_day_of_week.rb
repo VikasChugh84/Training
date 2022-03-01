d68e6c0ce43f080190eb6409e86d36a61154d440
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-01 13:27:47 +0530

    return day of specific date program 43

---
+++ Ruby/Ruby Programs/Assignment Programs/43_return_day_of_week.rb
@@ -0,0 +1,17 @@
+=begin
+  Write a function that, given a date (in the format MM/DD/YYYY), returns the day of the week as a string.
+  Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", or "Saturday".
+=end
+
+require 'date'
+
+def fetch_day(date)
+  date = Date.strptime(date, '%m/%d/%Y')
+  return date.strftime("%A")
+end
+
+print "Enter date: "
+date = gets.chomp!
+puts "day of week: #{fetch_day(date)}"
+
