46b682687fdde4489877edf17e8f894ef65d1da9
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-03 18:47:37 +0530

    understanding routes functinality

--- Rails/rails_apps/simple_cms/config/routes.rb
+++ Rails/rails_apps/simple_cms/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'demo#index'
+  # define demo index as root route. 
+
   get 'demo/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get ':controller(/:action)'
+  # default route structure
 
   # Defines the root path route ("/")
   # root "articles#index"
