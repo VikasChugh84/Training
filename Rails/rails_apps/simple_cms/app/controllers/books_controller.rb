dcf8283b277750a7d9c5ef2e550f3cbc8a78d74b
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-04 13:07:01 +0530

    Add action to understand the redirect functionality

--- Rails/rails_apps/simple_cms/app/controllers/books_controller.rb
+++ Rails/rails_apps/simple_cms/app/controllers/books_controller.rb
@@ -11,4 +11,16 @@ class BooksController < ApplicationController
     # Point here is demo is considered just as folder instead of controller.
   end
 
+  def check_book
+    redirect_to :controller => 'demo', :action => 'show'
+    # This can redirect request to show action of demo controller. 
+    
+  end
+
+  def check_subject
+    redirect_to controller: 'books', action: 'related_subject'
+    # This can redirect to check action of books controller
+    # Redirect could be written in above format as well.
+  end
+
 end
