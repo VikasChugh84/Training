61fb130df99e64e1c7f49842f6c06acb67c83c4b
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-04 12:09:50 +0530

    render template functionality

--- Rails/rails_apps/simple_cms/app/controllers/books_controller.rb
+++ Rails/rails_apps/simple_cms/app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
   
   def all_books_count
+    render ('books/related_subject')
+    # This can render to the related_subject template instead of all_books_count
   end
 
   def related_subject
+    render ('demo/show')
+    # This can render to show template of demo folder instead of related_subject
+    # Point here is demo is considered just as folder instead of controller.
   end
 
 end
