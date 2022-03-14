4ef6b7169a651a1645adaaddce513de09ea26bbe
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-14 13:29:12 +0530

    Add Image link

--- Rails/rails_apps/simple_cms/db/schema.rb
+++ Rails/rails_apps/simple_cms/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_14_050728) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_14_075105) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -169,6 +169,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_050728) do
     t.bigint "imageable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_link"
     t.index ["imageable_type", "imageable_id"], name: "index_pictures_on_imageable"
   end
 
