4766218ad7ba9423b3b474946d745e22a2489f97
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-16 18:16:12 +0530

    GET and POST Method Implementation through Postman

--- Rails/rails_api/weather_no_jwt/app/controllers/api/v1/locations_controller.rb
+++ Rails/rails_api/weather_no_jwt/app/controllers/api/v1/locations_controller.rb
@@ -1,15 +1,31 @@
 class Api::V1::LocationsController < ApplicationController
-  before_action :set_location
+  #before_action :set_location, except: :create
+
+  def index
+    @location = Location.all
+    render json: @location
+  end
 
   def show
-  	render json: @location
-    #request.format = :json
+    @location = Location.find(params[:id])
+  	render json: @location, status: 200
+  end
+
+  def create
+    @location = Location.new(location_params_permit)
+    if @location.save
+      redirect_to api_v1_locations_path
+    end
   end
 
   private
 
-  def set_location
-  	@location = Location.find(params[:id])
+  #def set_location
+  	#@location = Location.find(params[:id])
+  #end
+
+  def location_params_permit
+    params.permit(:name)
   end
 
 end
