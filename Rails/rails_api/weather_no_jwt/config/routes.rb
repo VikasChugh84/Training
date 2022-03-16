7f70bc06f5053420fba1e79fe6b53f04d35e3d61
Vikas Chugh <vikaschugh84@gmail.com>
2022-03-16 16:47:13 +0530

    App without JWT code integration

---
+++ Rails/rails_api/weather_no_jwt/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :locations do
+        resources :recordings
+      end
+    end
+  end
+  # Here the resources defined specifically for apis
+
+  #resources :locations
+  # Here the resources defined for basic rails app like controller and html files.
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
